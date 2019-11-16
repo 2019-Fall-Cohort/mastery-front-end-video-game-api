a04872eb1b1d09965a4c8ae33a12f776a1ca324c
robby <robby.remmenga@gmail.com>
2019-11-16 17:45:08 -0500

    changes

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -5,10 +5,10 @@ function GameCard(game) {
     return Deact.create("section", { class: `game-card` }, [
         Deact.create("h3", { class: `game-card__title` }, game.title),
         Deact.create("h5", { class: `game-card__release-date` }, game.releaseDate),
-        // Deact.create("h6", { class: `game-card__category` }, game.category),
+        // Deact.create("h5", { class: `game-card__category` }, game.category),
         Deact.create("img", { class: `game-card__imageUrl`, src: `${game.imageUrl}`, alt: `${game.title}` }, ""),
-        // Deact.create("h7", { class: `game-card__publisher`}, game.publisher),
-        // Deact.create("h8", { class:  `game-card__system` }, game.system)
+        // Deact.create("h5", { class: `game-card__publisher`}, game.publisher),
+        // Deact.create("h5", { class:  `game-card__system` }, game.system)
     ]);
 }
 
